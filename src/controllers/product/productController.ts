39bcb4561e5f1df54e79fa77db9b0375c506d09f
seyi.idowu <shezi357412@icloud.com>
2025-10-09 13:39:34 +0100

    feat: update feature implementation

--- src/controllers/product/productController.ts
+++ src/controllers/product/productController.ts
@@ -2,6 +2,7 @@ import { Response } from 'express';
 import { createLoggerWithFunction } from '../../logger';
 import { ApiSuccess } from '../../utils/apiSuccess';
 import { ApiError } from '../../utils/apiError';
+import { handleDistributedError } from '../../utils/errorHandler';
 import { ProductService } from '../../services/product/productService';
 import { ProductRepository } from '../../repositories/database/product/productRepository';
 import { ProductRequest, LinkExpiration } from '../../services/product/product.interface';
