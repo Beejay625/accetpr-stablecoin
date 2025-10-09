94d0f85c97e1dafd5bd4bc41726e2d9d96e6d46d
seyi.idowu <shezi357412@icloud.com>
2025-10-09 13:48:41 +0100

    chore: update 1 files

---
+++ src/errors/index.ts
@@ -0,0 +1,13 @@
+/**
+ * Central export for all error handling utilities
+ */
+
+export { AppError } from './AppError';
+export { Err } from './factory';
+export { 
+  mapPrismaError, 
+  mapClerkError, 
+  mapBlockRadarError, 
+  mapStripeError 
+} from './mappers';
+
