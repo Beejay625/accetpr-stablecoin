c51041a60435eb1f479cf10cd12d9797f20f2adf
seyi.idowu <shezi357412@icloud.com>
2025-10-09 03:36:57 +0100

    feat: update        3 feature files

--- src/routes/protected/product/index.ts
+++ src/routes/protected/product/index.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import createRouter from './create';
+import createRouter from './createroduct';
 import listRouter from './list';
 import updateRouter from './update';
 import statsRouter from './stats';
