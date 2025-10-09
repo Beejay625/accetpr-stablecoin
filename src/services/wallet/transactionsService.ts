5e72a92bba7cfadcb8fa4dc251d788c0981b0319
seyi.idowu <shezi357412@icloud.com>
2025-10-09 03:14:56 +0100

    feat: update feature implementation

--- src/services/wallet/transactionsService.ts
+++ src/services/wallet/transactionsService.ts
@@ -2,7 +2,7 @@ import { createLoggerWithFunction } from '../../logger';
 import { walletRepository } from '../../repositories/database/wallet';
 import { getAddressTransactions } from '../../providers/blockradar/transactions/walletTransactions';
 import { TransactionsResponse, SimplifiedTransaction } from '../../providers/blockradar/transactions/transactions.interface';
-import { DEFAULT_CHAINS } from '../../providers/blockradar/walletIdAndTokenManagement/chainsAndTokensHelpers';
+import { DEFAULT_CHAINS, isChainSupported } from '../../providers/blockradar/walletIdAndTokenManagement/chainsAndTokensHelpers';
 
 /**
  * Transactions Service
