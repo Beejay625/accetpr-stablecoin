7baea55ec8e673b3436e0ab197d928337a075634
seyi.idowu <shezi357412@icloud.com>
2025-10-09 03:15:12 +0100

    feat: update feature implementation

--- src/services/wallet/withdrawService.ts
+++ src/services/wallet/withdrawService.ts
@@ -2,7 +2,7 @@ import { createLoggerWithFunction } from '../../logger';
 import { walletRepository } from '../../repositories/database/wallet';
 import { withdrawFromAddress } from '../../providers/blockradar/withdraw/walletWithdraw';
 import { WithdrawResponse, SingleWithdrawRequest, BatchWithdrawRequest } from '../../providers/blockradar/withdraw/withdraw.interface';
-import { DEFAULT_CHAINS } from '../../providers/blockradar/walletIdAndTokenManagement/chainsAndTokensHelpers';
+import { DEFAULT_CHAINS, isChainSupported } from '../../providers/blockradar/walletIdAndTokenManagement/chainsAndTokensHelpers';
 import { validateSingleWithdrawRequest, validateBatchWithdrawRequest } from './helpers/validateWithdrawal';
 
 /**
