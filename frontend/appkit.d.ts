c5245d553a14c78a920842f7f846c121352f04a9
seyi.idowu <shezi357412@icloud.com>
2025-11-09 20:44:22 +0100

    docs: install viem for Ethereum utilities

---
+++ frontend/appkit.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for AppKit web components
+declare namespace JSX {
+  interface IntrinsicElements {
+    'appkit-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>
+  }
+}
+
