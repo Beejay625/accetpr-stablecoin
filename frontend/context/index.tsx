7afd163bddf09733b634f8ed44c77a79cfcaf702
seyi.idowu <shezi357412@icloud.com>
2025-11-09 20:32:03 +0100

    feat: setup AppKit adapter with wagmi

---
+++ frontend/context/index.tsx
@@ -0,0 +1,51 @@
+'use client'
+
+import { wagmiAdapter, projectId } from '@/config'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { createAppKit } from '@reown/appkit/react'
+import { mainnet, arbitrum, base, baseSepolia } from '@reown/appkit/networks'
+import React, { type ReactNode } from 'react'
+import { cookieToInitialState, WagmiProvider, type Config } from 'wagmi'
+
+// Set up queryClient
+const queryClient = new QueryClient()
+
+if (!projectId) {
+  throw new Error('Project ID is not defined')
+}
+
+// Set up networks based on environment
+const isDev = process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'dev'
+const networks = isDev ? [mainnet, arbitrum, base] : [baseSepolia]
+
+// Set up metadata
+const metadata = {
+  name: 'StableStack',
+  description: 'StableStack Wallet Management',
+  url: typeof window !== 'undefined' ? window.location.origin : 'https://stablestack.com',
+  icons: ['https://avatars.githubusercontent.com/u/179229932']
+}
+
+// Create the modal
+const modal = createAppKit({
+  adapters: [wagmiAdapter],
+  projectId,
+  networks,
+  defaultNetwork: networks[0],
+  metadata: metadata,
+  features: {
+    analytics: true // Optional - defaults to your Cloud configuration
+  }
+})
+
+function ContextProvider({ children, cookies }: { children: ReactNode; cookies: string | null }) {
+  const initialState = cookieToInitialState(wagmiAdapter.wagmiConfig as Config, cookies)
+  return (
+    <WagmiProvider config={wagmiAdapter.wagmiConfig as Config} initialState={initialState}>
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    </WagmiProvider>
+  )
+}
+
+export default ContextProvider
+
