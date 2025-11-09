86f05fa34871fde55667588dc085261db598f9a8
seyi.idowu <shezi357412@icloud.com>
2025-11-09 20:33:36 +0100

    chore: add WalletConnect reconnection logic

---
+++ frontend/app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { headers } from 'next/headers'
+import { ClerkProvider } from '@clerk/nextjs'
+import ContextProvider from '@/context'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'StableStack - Wallet Management',
+  description: 'Powered by Reown AppKit',
+}
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  const headersObj = await headers()
+  const cookies = headersObj.get('cookie')
+
+  return (
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ContextProvider cookies={cookies}>{children}</ContextProvider>
+        </body>
+      </html>
+    </ClerkProvider>
+  )
+}
+
